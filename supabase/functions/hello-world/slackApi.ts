f17ee7ab36d796d295be7405015d47952ab32b35
Marek Lisy <lisy@shoptet.cz>
2025-03-12 10:22:39 +0100

    add debug mode for safer testing

--- supabase/functions/hello-world/slackApi.ts
+++ supabase/functions/hello-world/slackApi.ts
@@ -1,6 +1,10 @@
 import ky from "https://esm.sh/ky@1.7.5";
 
 export async function postToSlack(message: string) {
+  if (Deno.env.get("DEBUG") === "1") {
+    console.log("DEBUG: Would post message to Slack:\n", message);
+    return;
+  }
   try {
     await ky.post(
       `https://hooks.slack.com/services/${Deno.env.get("SLACK_HOOK_TOKEN")}`,
